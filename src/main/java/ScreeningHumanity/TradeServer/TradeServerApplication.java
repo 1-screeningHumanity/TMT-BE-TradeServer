6590d287c74942183e4ef046f95514500617deff
KangBaekGwa <ksu9801@gmail.com>
2024-09-20 00:09:01 +0900

    Refactor : Test용 YML 생성 및 프로파일 구분 진행.

--- src/main/java/ScreeningHumanity/TradeServer/TradeServerApplication.java
+++ src/main/java/ScreeningHumanity/TradeServer/TradeServerApplication.java
@@ -2,14 +2,14 @@ package ScreeningHumanity.TradeServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableJpaAuditing
-@EnableFeignClients
+//@EnableDiscoveryClient //별도의 Config로 분기 처리. 사유 : Mvc Test or SpringBootTest 시, Bean등록 처리됨.
+//@EnableJpaAuditing //별도의 Config로 분기 처리. 사유 : Mvc Test or SpringBootTest 시, Bean등록 처리됨.
+//@EnableFeignClients //별도의 Config로 분기 처리. 사유 : Mvc Test or SpringBootTest 시, Bean등록 처리됨.
 public class TradeServerApplication {
 
 	public static void main(String[] args) {
